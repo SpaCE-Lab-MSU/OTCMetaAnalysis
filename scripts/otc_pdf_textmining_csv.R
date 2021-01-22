a53444a4cee3fa0ea646c30b80daa0b526553921
Kara Dobson <dobsonk2@msu.edu>
2021-01-22 15:39:15 -0500

    trying a different method for csv

--- scripts/otc_pdf_textmining_csv.R
+++ scripts/otc_pdf_textmining_csv.R
@@ -31,63 +31,60 @@ rm(list=ls())
 # load packages
 library(tm)
 library(pdftools)
-
-loop <- function(csv_file){
+    
+# function to scan pdfs
+scan_pdfs <- function(csv_file){
   
-  for(i in (csv_file)){
-    
-  # function to scan pdfs
-  scan_pdfs <- function(i){
-    
-    # function to read in PDFs and maintain layout
-    Rpdf <- readPDF(engine = "Rpoppler")
-    
-    # read in the PDFs, convert to text
-    msu.pdfs.data <- Corpus(URISource(i), 
-                            readerControl = list(reader = Rpdf))
-    
-    # search through the papers for specific terms
-    otc.msu <- DocumentTermMatrix(msu.pdfs.data,
-                                  list(dictionary = c("chamber","chambers",
-                                                      "open-top","open top",
-                                                      "warming chamber","warming chambers",
-                                                      "warming-chamber","warming chambers",
-                                                      "passive","passively","passive-","passively-",
-                                                      "temperature","temperatures","ITEX","itex",
-                                                      "fan","fans","plant","plants",
-                                                      "seedling","seedlings","sapling","saplings",
-                                                      "shrub","shrubs","grass","grasses",
-                                                      "sedge","sedges","forb","forbs",
-                                                      "tree","trees","vegetation")))
-    
-    otc.msu <- otc.msu[slam::row_sums(otc.msu) > 0,
-                       slam::col_sums(otc.msu) > 0]
-    
-    otc.msu <- data.frame(docs = row.names(otc.msu), as.matrix(otc.msu), row.names = NULL)
-    
-    # column headers with spaces become (.) or with dashes become (.1):
-    names(otc.msu)
-    
-    # now some code to determine the high priority papers for using in the meta-analysis:
-    # by default, paper is 0 priority unless meets following criteria:
-    dim(otc.msu)
-    otc.msu$priority<-0
-    otc.msu$priority[otc.msu$chamber>0]<-1
-    otc.msu$priority[otc.msu$chambers>0]<-1
-    otc.msu$priority[otc.msu$open.top>0]<-1
-    otc.msu$priority[otc.msu$open.top.1>0]<-1
-    otc.msu$priority[otc.msu$warming.chamber>0]<-1
-    otc.msu$priority[otc.msu$warming.chamber.1>0]<-1
-    otc.msu$passiveotc<-rowSums(cbind(otc.msu$passive, otc.msu$open.top, otc.msu$open.top.1))
-    otc.msu$priority[otc.msu$passiveotc>0]<-1
-    
-    # keep only the paper names + priority columns
-    otc.msu <- subset(otc.msu, priority == 1)
-    otc.msu <- subset(otc.msu, select = c("docs", "priority"))
-    
-    return(otc.msu)
-  }
-  }
+  # specify file path from csv file
+  msu.pdfs <- list.files(path=csv_file, pattern="pdf$")
+  
+  # function to read in PDFs and maintain layout
+  Rpdf <- readPDF(engine = "Rpoppler")
+  
+  # read in the PDFs, convert to text
+  msu.pdfs.data <- Corpus(URISource(i), 
+                          readerControl = list(reader = Rpdf))
+  
+  # search through the papers for specific terms
+  otc.msu <- DocumentTermMatrix(msu.pdfs.data,
+                                list(dictionary = c("chamber","chambers",
+                                                    "open-top","open top",
+                                                    "warming chamber","warming chambers",
+                                                    "warming-chamber","warming chambers",
+                                                    "passive","passively","passive-","passively-",
+                                                    "temperature","temperatures","ITEX","itex",
+                                                    "fan","fans","plant","plants",
+                                                    "seedling","seedlings","sapling","saplings",
+                                                    "shrub","shrubs","grass","grasses",
+                                                    "sedge","sedges","forb","forbs",
+                                                    "tree","trees","vegetation")))
+  
+  otc.msu <- otc.msu[slam::row_sums(otc.msu) > 0,
+                     slam::col_sums(otc.msu) > 0]
+  
+  otc.msu <- data.frame(docs = row.names(otc.msu), as.matrix(otc.msu), row.names = NULL)
+  
+  # column headers with spaces become (.) or with dashes become (.1):
+  names(otc.msu)
+  
+  # now some code to determine the high priority papers for using in the meta-analysis:
+  # by default, paper is 0 priority unless meets following criteria:
+  dim(otc.msu)
+  otc.msu$priority<-0
+  otc.msu$priority[otc.msu$chamber>0]<-1
+  otc.msu$priority[otc.msu$chambers>0]<-1
+  otc.msu$priority[otc.msu$open.top>0]<-1
+  otc.msu$priority[otc.msu$open.top.1>0]<-1
+  otc.msu$priority[otc.msu$warming.chamber>0]<-1
+  otc.msu$priority[otc.msu$warming.chamber.1>0]<-1
+  otc.msu$passiveotc<-rowSums(cbind(otc.msu$passive, otc.msu$open.top, otc.msu$open.top.1))
+  otc.msu$priority[otc.msu$passiveotc>0]<-1
+  
+  # keep only the paper names + priority columns
+  otc.msu <- subset(otc.msu, priority == 1)
+  otc.msu <- subset(otc.msu, select = c("docs", "priority"))
+  
+  return(otc.msu)
 }
 
 # set arguments
@@ -96,7 +93,7 @@ args = commandArgs(trailingOnly=TRUE)
 if (length(args) == 2) {
   
   # since there were arguments on the command line use those to run the function
-  otc_results <- loop(args[1])
+  otc_results <- scan_pdfs(args[1])
   write.csv(otc_results, file=args[2])
   
 } else {
