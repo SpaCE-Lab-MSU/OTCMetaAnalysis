c4a1c1c185af8bc4e0a876d3f8134a26ef4563a4
Kara Dobson <dobsonk2@msu.edu>
2021-03-11 17:44:46 -0500

    updating search terms

--- scripts/otc_pdf_textmining_csv.R
+++ scripts/otc_pdf_textmining_csv.R
@@ -74,8 +74,8 @@ scan_pdfs <- function(csv_file){
   otc.msu$priority<-0
   otc.msu$priority[otc.msu$open.top.chamber>0]<-1
   otc.msu$priority[otc.msu$open.top.chamber.1>0]<-1
-  otc.msu$priority[otc.msu$open.top>0]<-1
-  otc.msu$priority[otc.msu$open.top.1>0]<-1
+  #otc.msu$priority[otc.msu$open.top>0]<-1
+  #otc.msu$priority[otc.msu$open.top.1>0]<-1
   otc.msu$priority[otc.msu$otc>0]<-1
   otc.msu$priority[otc.msu$itex>0]<-1
   #otc.msu$passiveotc<-rowSums(cbind(otc.msu$passive, otc.msu$open.top, otc.msu$open.top.1))
