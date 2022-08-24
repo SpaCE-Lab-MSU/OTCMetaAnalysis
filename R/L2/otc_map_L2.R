91af59336575403bdb7e39e9bb43274eefb339c2
dobsonk2 <65179695+dobsonk2@users.noreply.github.com>
2022-08-24 10:21:21 -0400

    map with each otc study location

---
+++ R/L2/otc_map_L2.R
@@ -0,0 +1,32 @@
+# TITLE:          Map of OTC studies
+# AUTHORS:        Kara Dobson
+# COLLABORATORS:  Phoebe Zarnetske, Pat Bills
+# DATA INPUT:     An L1 dataframe with latitude and longitude of studies
+# DATA OUTPUT:    Map containing points for each study location
+# DATE:           Aug 2022
+
+#https://datavizpyr.com/how-to-make-world-map-with-ggplot2-in-r/
+# Load packages
+library(tidyverse)
+
+# set working directory
+MA_dir<-Sys.getenv("MADIR")
+
+# Load data
+coord <- read.csv(file.path(MA_dir,"L1/otc_data_coordinates_L1.csv"))
+world <- map_data("world")
+
+# make map
+ggplot() +
+  geom_map(
+    data = world, map = world,
+    aes(long, lat, map_id = region),
+    color = "black", fill = "lightgray", size = 0.1
+  ) +
+  geom_point(
+    data = coord,
+    aes(Long, Lat),
+    alpha = 0.7,
+    color = "red"
+  ) +
+  theme_classic()
