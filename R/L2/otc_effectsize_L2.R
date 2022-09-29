b424c00457889f4b6c6355eb8076e9adb5a1c5f9
dobsonk2 <65179695+dobsonk2@users.noreply.github.com>
2022-09-29 14:30:02 -0400

    prelim effect size code

--- R/L2/otc_effectsize_L2.R
+++ R/L2/otc_effectsize_L2.R
@@ -17,7 +17,18 @@ otc_data <- read.csv(file.path(MA_dir,"L1/otc_data_sample_sizes.csv"))
 
 # example code for calculating effect sizes (eventually)
 # https://rfunctions.blogspot.com/2016/10/meta-analysis-in-r.html
-esmd <- escalc(measure="MD", m1i=Warmed_Mean, m2i=Ambient_Mean,
+esmd <- escalc(measure="ROM", m1i=Warmed_Mean, m2i=Ambient_Mean,
                sd1i=Warmed_Var, sd2i=Ambient_Var,
                n1i=Warmed_N, n2i=Ambient_N,
                data=otc_data)
+
+# https://ecologyforacrowdedplanet.wordpress.com/2013/05/10/using-metafor-and-ggplot-togetherpart-1/
+# example stats: using biomass for now
+esmd_bio_above <- esmd %>%
+  filter(Var_type == "Biomass_above")
+# running stats
+SMD.ma<-rma.uni(yi,vi,method="REML",data=esmd_bio_above)
+summary(SMD.ma)
+# basic plot
+forest.rma(SMD.ma)
+
