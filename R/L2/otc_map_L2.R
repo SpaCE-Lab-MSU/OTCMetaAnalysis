b6efb8df726792893c001c76dba59e0f0b1192fc
dobsonk2 <65179695+dobsonk2@users.noreply.github.com>
2023-03-20 13:51:58 -0400

    updating map of study sites

--- R/L2/otc_map_L2.R
+++ R/L2/otc_map_L2.R
@@ -15,30 +15,21 @@ MA_dir<-Sys.getenv("MADIR")
 
 # Load data
 coord <- read.csv(file.path(MA_dir,"L1/otc_data_coordinates_L1.csv"))
+effect <- read.csv(file.path(MA_dir,"L2/otc_effect_sizes_L2.csv"))
 world <- map_data("world")
 
-# removing coordinates for pubs that were removed
+
+# only keeping pub numbers in coord data that appear in the effect size data
+coord$keep_lat <- coord$Pub_number %in% c(effect$Pub_number)
 coord <- coord %>%
-  filter(!(Pub_number == 4 | Pub_number == 5 | Pub_number == 19 | Pub_number == 23 |
-           Pub_number == 28 | Pub_number == 31 | Pub_number == 35 | Pub_number == 36 |
-           Pub_number == 37 | Pub_number == 39 | Pub_number == 49 | Pub_number == 52 |
-           Pub_number == 5 | Pub_number == 58 | Pub_number == 67 | Pub_number == 68 |
-           Pub_number == 72 | Pub_number == 76 | Pub_number == 79 | Pub_number == 84 |
-           Pub_number == 89 | Pub_number == 125 | Pub_number == 142 | Pub_number == 143 |
-           Pub_number == 146 | Pub_number == 154 | Pub_number == 155 | Pub_number == 156 |
-           Pub_number == 162 | Pub_number == 177 | Pub_number == 179 |
-           Pub_number == 181 | Pub_number == 183 | Pub_number == 185 | Pub_number == 189 |
-           Pub_number == 197 | Pub_number == 198 | Pub_number == 202 | Pub_number == 206 |
-           Pub_number == 208 | Pub_number == 212 | Pub_number == 214 | Pub_number == 215 |
-           Pub_number == 217 | Pub_number == 220 | Pub_number == 226 | Pub_number == 228 |
-           Pub_number == 232 | Pub_number == 234))
+  filter(!(keep_lat == "FALSE"))
 
 # make map
 png("otc_plot_L2.png", units="in", width=10, height=6, res=300)
 ggplot() +
   geom_map(
     data = world, map = world,
-    aes(long, lat, map_id = region),
+    aes(map_id = region),
     color = "black", fill = "lightgray", size = 0.1
   ) +
   geom_point(
