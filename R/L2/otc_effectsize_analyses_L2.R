864868c6f7d1ecd0b73a7d290096a1bab3628f83
dobsonk2 <65179695+dobsonk2@users.noreply.github.com>
2023-03-20 13:51:51 -0400

    updating effect size plots and analyses

--- R/L2/otc_effectsize_analyses_L2.R
+++ R/L2/otc_effectsize_analyses_L2.R
@@ -13,6 +13,7 @@ rm(list = ls())
 library(tidyverse)
 library(metafor)
 library(plotrix)
+library(multcomp)
 
 # set working directory
 MA_dir<-Sys.getenv("MADIR")
@@ -58,7 +59,22 @@ res.rma.months
 # no, so we don't need to account for months warmed
 
 
-### main stats: update var_type based on what variable to look at
+
+### main stats (all variables):
+# removing func groups that are blank
+esmd_clean2<- esmd_clean %>%
+  filter(!(Var_type == ""))
+# equal effects model for height
+res.rma.all <- rma(yi, vi, mods=~Var_type, data=esmd_clean2)
+res.rma.all
+# all comparisons
+# Values of P=1.0 indicate that there is no evidence of a difference between variables
+# Holm correction used here because it was used in the Kuebbing phenology meta-analysis
+summary(glht(res.rma.all, linfct=cbind(contrMat(rep(1,31), type="Tukey"))), test=adjusted("holm"))
+
+
+
+### main stats (single variable): update var_type based on what variable to look at
 unique(esmd_clean$Var_type)
 esmd_clean %>% 
   count(Var_type)
@@ -66,7 +82,89 @@ esmd_clean %>%
 ## height
 esmd_height <- esmd_clean %>%
   filter(Var_type == "Height")
-# equal effects model for height
-res.rma.height <- rma(yi, vi, data=esmd_height, method="EE")
+# removing func groups that are blank
+esmd_height <- esmd_height %>%
+  filter(!(Func_group_broad == ""))
+# models for height
+res.rma.height <- rma(yi, vi, mods=~Func_group_broad-1, data=esmd_height)
 res.rma.height
+res.rma.height2 <- rma(yi, vi, data=esmd_height)
+res.rma.height2
+res.rma.height3 <- rma(yi, vi, mods=~Latitude,data=esmd_height)
+res.rma.height3
+# all comparisons - needs some work
+summary(glht(res.rma.height, linfct=cbind(contrMat(rep(1,9), type="Tukey"))), test=adjusted("none"))
 
+## biomass
+esmd_ab_biomass <- esmd_clean %>%
+  filter(Var_type == "Biomass_above")
+# removing func groups that are blank
+esmd_ab_biomass <- esmd_ab_biomass %>%
+  filter(!(Func_group_broad == ""))
+# models for height
+res.rma.ab.biomass <- rma(yi, vi, mods=~Func_group_broad-1, data=esmd_ab_biomass)
+res.rma.ab.biomass
+res.rma.ab.biomass2 <- rma(yi, vi, data=esmd_ab_biomass)
+res.rma.ab.biomass2
+res.rma.ab.biomass3 <- rma(yi, vi, mods=~Latitude,data=esmd_ab_biomass)
+res.rma.ab.biomass3
+# all comparisons - needs some work
+summary(glht(res.rma.ab.biomass, linfct=cbind(contrMat(rep(1,9), type="Tukey"))), test=adjusted("none"))
+
+## percent cover
+esmd_cover <- esmd_clean %>%
+  filter(Var_type == "Percent_cover")
+# removing func groups that are blank
+esmd_cover <- esmd_cover %>%
+  filter(!(Func_group_broad == ""))
+# models for height
+res.rma.cover <- rma(yi, vi, mods=~Func_group_broad-1, data=esmd_cover)
+res.rma.cover
+res.rma.cover2 <- rma(yi, vi, data=esmd_cover)
+res.rma.cover2
+res.rma.cover3 <- rma(yi, vi, mods=~Latitude,data=esmd_cover)
+res.rma.cover3
+# all comparisons - needs some work
+summary(glht(res.rma.cover, linfct=cbind(contrMat(rep(1,9), type="Tukey"))), test=adjusted("none"))
+
+## nitrogen
+esmd_ab_n <- esmd_clean %>%
+  filter(Var_type == "Nitrogen_above")
+# removing func groups that are blank
+esmd_ab_n <- esmd_ab_n %>%
+  filter(!(Func_group_broad == ""))
+# models for height
+res.rma.ab.n <- rma(yi, vi, mods=~Func_group_broad-1, data=esmd_ab_n)
+res.rma.ab.n
+res.rma.ab.n2 <- rma(yi, vi, data=esmd_ab_n)
+res.rma.ab.n2
+res.rma.ab.n3 <- rma(yi, vi, mods=~Latitude,data=esmd_ab_n)
+res.rma.ab.n3
+# all comparisons - needs some work
+summary(glht(res.rma.ab.n, linfct=cbind(contrMat(rep(1,9), type="Tukey"))), test=adjusted("none"))
+
+
+
+### test analysis using example data
+# tpos and tneg - number of pos and neg tuberculosis cases for vaccinated individuals
+# cpos and cneg - number of pos and neg tuberculosis cases for unvaccinated individuals
+dat <- escalc(measure = "RR", ai = tpos, bi = tneg, ci = cpos,
+              di = cneg, data = dat.bcg, append = TRUE)
+res <- rma(yi, vi, data = dat)
+res
+# the estimated average log relative risk is equal to ˆµ = −0.7145 (95% CI:
+# −1.0669 to −0.3622). For easier interpretation, it may be useful to transform these values
+# back to the relative risk scale through exponentiation (i.e., exp(ˆµ) = 0.49 with 95% CI: 0.34
+# to 0.70). The results therefore suggest that the risk of a tuberculosis infection in vaccinated
+# individuals is on average half as large as the infection risk without the vaccination. The null
+# hypothesis H0 : µ = 0 can be clearly rejected (z = −3.97, p < 0.0001).
+res <- rma(yi, vi, mods = ~ ablat + year, data = dat)
+res
+# The estimated amount of residual heterogeneity is equal to tau^2 = 0.1108, suggesting that
+# (0.3132 − 0.1108)/.3132 = 65% of the total amount of heterogeneity can be accounted for by
+# including the two moderators in the model. However, while we can reject H0 : β1 = β2 = 0
+# based on the omnibus test (QM = 12.20, df = 2, p < 0.01), only absolute latitude appears
+# to have a significant influence on the effectiveness of the vaccine (i.e., for H0 : β1 = 0, we find
+# z = −2.74 with p < 0.01, while for H0 : β2 = 0, we find z = 0.13 with p = 0.90). The test for
+# residual heterogeneity is significant (QE = 28.33, df = 10, p < 0.01), possibly indicating that
+# other moderators not considered in the model are influencing the vaccine effectiveness.
--- R/L2/otc_effectsize_plots_L2.R
+++ R/L2/otc_effectsize_plots_L2.R
@@ -31,35 +31,34 @@ esmd <- escalc(measure="SMD", m1i=Warmed_Mean, m2i=Ambient_Mean, # SMD = Hedge's
 esmd_clean <- esmd %>%
   filter(!is.na(vi))
 
+# uploading the effect size data
+write.csv(esmd_clean, file.path(MA_dir,"L2/otc_effect_sizes_L2.csv"), row.names=F)
+
 # https://ecologyforacrowdedplanet.wordpress.com/2013/05/10/using-metafor-and-ggplot-togetherpart-1/
 # example stats: update var_type based on what variable to look at
-unique(esmd_clean$Var_type)
+unique(esmd_clean$Var_type_broad)
 unique(esmd_clean$Pub_number)
 esmd_clean %>% 
-  count(Var_type)
+  count(Var_type_broad)
 
 esmd_var_type <- esmd_clean %>%
-  filter(Var_type == "Height" | Var_type == "Biomass_above" | Var_type == "Biomass_below" |
-           Var_type == "Percent_cover" | Var_type == "Nitrogen_above" | Var_type == "Shoot_length" |
-           Var_type == "SLA" | Var_type == "Leaf_length" | Var_type == "Flower_num")
-
-esmd_single_var <- esmd_clean %>%
-  filter(Var_type == "Biomass_above")
+  filter(Var_type_broad == "Biomass_above" | Var_type_broad == "Biomass_below"
+         | Var_type_broad == "Percent_cover" | Var_type_broad == "Height"
+         | Var_type_broad == "Flower_num" | Var_type_broad == "Nitrogen_above")
 
 esmd_var_phen <- esmd_clean %>%
-  filter(Var_type == "Phen_abscission" | Var_type == "Phen_bud_break" | Var_type == "Phen_flwr" |
-           Var_type == "Phen_flwr_lifespan" | Var_type == "Phen_seed_set")
+  filter(Var_type_broad == "Phen_early" | Var_type_broad == "Phen_late")
 
 # basic plot
 forest.rma(SMD.ma)
 
 # all data sorted by variable type
 esmd_var_type_sum <- esmd_clean %>%
-  group_by(Var_type) %>%
+  group_by(Var_type_broad) %>%
   summarize(avg = mean(yi, na.rm = TRUE),
             se = std.error(yi, na.rm = TRUE))
 png("effect.png", units="in", width=8, height=6, res=300)
-ggplot(esmd_var_type_sum, aes(y = reorder(Var_type, -avg, FUN=mean), x = avg)) +
+ggplot(esmd_var_type_sum, aes(y = reorder(Var_type_broad, -avg, FUN=mean), x = avg)) +
   #facet_wrap(.~Var_type) +
   geom_point(shape = 18, size = 4) +  
   geom_errorbarh(aes(xmin = avg - se, xmax = avg + se), height = 0.25) +
@@ -80,12 +79,12 @@ dev.off()
 
 # selected variables sorted by functional group
 esmd_var_type_sum2 <- esmd_var_type %>%
-  group_by(Var_type, Func_group_broad) %>%
+  group_by(Var_type_broad, Func_group_broad) %>%
   summarize(avg = mean(yi, na.rm = TRUE),
             se = std.error(yi, na.rm = TRUE))
-png("effect.png", units="in", width=8, height=6, res=300)
+png("effect_func.png", units="in", width=8, height=6, res=300)
 ggplot(esmd_var_type_sum2, aes(y = Func_group_broad, x = avg)) +
-  facet_wrap(.~Var_type) +
+  facet_wrap(.~Var_type_broad) +
   geom_point(shape = 18, size = 4) +  
   geom_errorbarh(aes(xmin = avg - se, xmax = avg + se), height = 0.25) +
   geom_vline(xintercept = 0, color = "red", linetype = "dashed", cex = 1, alpha = 0.5) +
@@ -105,7 +104,7 @@ dev.off()
 
 # regression between latitude and effect size
 ggplot(esmd_var_type, aes(x = Latitude, y = yi)) +
-  facet_wrap(.~Var_type) +
+  facet_wrap(.~Var_type_broad) +
   geom_point(size = 2) +
   geom_smooth(method = 'lm') +
   xlab("Latitude") +
