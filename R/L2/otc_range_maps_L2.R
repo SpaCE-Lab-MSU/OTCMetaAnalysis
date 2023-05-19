db64133430e9b3066ed5cd8f08eda20a9413da1e
dobsonk2 <65179695+dobsonk2@users.noreply.github.com>
2023-05-19 09:56:43 -0400

    updating range map code

--- R/L2/otc_range_maps_L2.R
+++ R/L2/otc_range_maps_L2.R
@@ -53,7 +53,6 @@ points(cbind(Carex_bigelowii$Longitude,Carex_bigelowii$Latitude),
        col = "blue",
        pch = 20,
        cex = 1)
-
 # Betula nana (second most common species)
 Betula_nana <- effect %>%
   filter(Genus_Species == "Betula_nana")
@@ -69,7 +68,6 @@ points(cbind(Betula_nana$Longitude,Betula_nana$Latitude),
        col = "blue",
        pch = 20,
        cex = 1)
-
 # Lonicera hispida (less common species)
 Lonicera_hispida <- effect %>%
   filter(Genus_Species == "Lonicera_hispida")
@@ -88,35 +86,104 @@ points(cbind(Lonicera_hispida$Longitude,Lonicera_hispida$Latitude),
 
 
 
+
 ### occurrence data - BIEN ###
+# Carex bigelowii
+Carex_bigelowii <- effect %>%
+  filter(Genus_Species == "Carex_bigelowii")
 Carex_bigelowii_full <- BIEN_occurrence_species(species = "Carex bigelowii",
                                                     cultivated = TRUE,
                                                     all.taxonomy = TRUE,
                                                     native.status = TRUE,
                                                     observation.type = TRUE,
                                                     political.boundaries = TRUE)
-# test map
-map('world', fill = TRUE, col= "grey", bg = "light blue") 
-points(cbind(Carex_bigelowii_full$longitude,
-             Carex_bigelowii_full$latitude),
-       col = "red",
-       pch = 20,
-       cex = 1) 
-points(cbind(Carex_bigelowii$Longitude,
-             Carex_bigelowii$Latitude),
-       col = "blue",
-       pch = 20,
-       cex = 1) 
-# better map
+# Betula nana
+Betula_nana <- effect %>%
+  filter(Genus_Species == "Betula_nana")
+Betula_nana_full <- BIEN_occurrence_species(species = "Betula nana",
+                                                cultivated = TRUE,
+                                                all.taxonomy = TRUE,
+                                                native.status = TRUE,
+                                                observation.type = TRUE,
+                                                political.boundaries = TRUE)
+# Deschampsia antarctica
+Deschampsia_antarctica <- effect %>%
+  filter(Genus_Species == "Deschampsia_antarctica")
+Deschampsia_antarctica_full <- BIEN_occurrence_species(species = "Deschampsia antarctica",
+                                            cultivated = TRUE,
+                                            all.taxonomy = TRUE,
+                                            native.status = TRUE,
+                                            observation.type = TRUE,
+                                            political.boundaries = TRUE)
+
+# Lonicera hispida
+Lonicera_hispida <- effect %>%
+  filter(Genus_Species == "Lonicera_hispida")
+Lonicera_hispida_full <- BIEN_occurrence_species(species = "Lonicera hispida",
+                                                       cultivated = TRUE,
+                                                       all.taxonomy = TRUE,
+                                                       native.status = TRUE,
+                                                       observation.type = TRUE,
+                                                       political.boundaries = TRUE)
+Hylocomium_splendens <- effect %>%
+  filter(Genus_Species == "Hylocomium_splendens")
+Hylocomium_splendens_full <- BIEN_occurrence_species(species = "Hylocomium splendens",
+                                                 cultivated = TRUE,
+                                                 all.taxonomy = TRUE,
+                                                 native.status = TRUE,
+                                                 observation.type = TRUE,
+                                                 political.boundaries = TRUE)
+
+
+
+# setting up map parameters
 mp <- map('world', fill = TRUE, plot=F) 
 SP <- map2SpatialPolygons(mp, IDs = mp$names, 
                           proj4string = CRS("+proj=longlat +datum=WGS84"))
 DATA <- data.frame(seq_len(length(SP)), row.names = names(SP))
 SPDF <- SpatialPolygonsDataFrame(SP, data = DATA)
+
+# mapping
+# Carex bigelowii
 spplot(SPDF, col.regions = "transparent", colorkey = FALSE,
        par.settings = list(axis.line = list(col = "transparent"))) +
   layer(panel.points(longitude, latitude, col="blue", pch=19), data=Carex_bigelowii_full) +
   layer(panel.points(Longitude, Latitude, col="red", pch=19), data=Carex_bigelowii)
+# Betula nana
+spplot(SPDF, col.regions = "transparent", colorkey = FALSE,
+       par.settings = list(axis.line = list(col = "transparent"))) +
+  layer(panel.points(longitude, latitude, col="blue", pch=19), data=Betula_nana_full) +
+  layer(panel.points(Longitude, Latitude, col="red", pch=19), data=Betula_nana)
+# Deschampsia antarctica
+spplot(SPDF, col.regions = "transparent", colorkey = FALSE,
+       par.settings = list(axis.line = list(col = "transparent"))) +
+  layer(panel.points(longitude, latitude, col="blue", pch=19), data=Deschampsia_antarctica_full) +
+  layer(panel.points(Longitude, Latitude, col="red", pch=19), data=Deschampsia_antarctica)
+# Lonicera hispida
+spplot(SPDF, col.regions = "transparent", colorkey = FALSE,
+       par.settings = list(axis.line = list(col = "transparent"))) +
+  layer(panel.points(longitude, latitude, col="blue", pch=19), data=Lonicera_hispida_full) +
+  layer(panel.points(Longitude, Latitude, col="red", pch=19), data=Lonicera_hispida)
+# Hylocomium splendens
+spplot(SPDF, col.regions = "transparent", colorkey = FALSE,
+       par.settings = list(axis.line = list(col = "transparent"))) +
+  layer(panel.points(longitude, latitude, col="blue", pch=19), data=Hylocomium_splendens_full) +
+  layer(panel.points(Longitude, Latitude, col="red", pch=19), data=Hylocomium_splendens)
+
+# old map
+#map('world', fill = TRUE, col= "grey", bg = "light blue") 
+#points(cbind(Carex_bigelowii_full$longitude,
+#             Carex_bigelowii_full$latitude),
+#       col = "red",
+#       pch = 20,
+#       cex = 1) 
+#points(cbind(Carex_bigelowii$Longitude,
+#             Carex_bigelowii$Latitude),
+#       col = "blue",
+#       pch = 20,
+#       cex = 1) 
+
+
 
 
 ### occurrence data - GBIF ###
