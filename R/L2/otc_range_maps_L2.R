294d57bc1b0962f72601121c954989cd5b4c2ac5
dobsonk2 <65179695+dobsonk2@users.noreply.github.com>
2023-05-22 14:32:02 -0400

    sorting through spp

--- R/L2/otc_range_maps_L2.R
+++ R/L2/otc_range_maps_L2.R
@@ -30,10 +30,171 @@ MA_dir<-Sys.getenv("MADIR")
 # effect size data
 effect <- read.csv(file.path(MA_dir,"L2/otc_effect_sizes_L2.csv"))
 
-# most common species?
+# list all species + their counts
+# this is just to see which species are the most common
 names(which.max(table(effect$Genus_Species)))
 count <- effect %>% 
   count(Genus_Species)
+
+# list of all species
+# first removing species that aren't named or only contain genus
+effect2 <- effect %>%
+  filter(!(Genus_Species == "_" | Genus_Species == "Sphagnum_" | Genus_Species == "Draba_" |
+             Genus_Species == "Carex_" | Genus_Species == "Oxytropis_" | Genus_Species == "Luzula_" |
+             Genus_Species == "Stereocaulon_" | Genus_Species == "Cladonia_" | Genus_Species == "Nephroma_" |
+             Genus_Species == "Peltigera_"))
+# replace underscore with space
+effect2$Genus_Species<-gsub("_", " ", effect2$Genus_Species, fixed=TRUE)
+# store each species in a list
+species <- as.list(unique(effect2$Genus_Species))
+
+# subset species list into chunks to run through the BIEN occurrence function
+species2 <- species[1:5]
+species3 <- species[6:10]
+species4 <- species[11:15]
+species5 <- species[16:20]
+species6 <- species[21:25]
+
+
+
+### occurrence data - BIEN ###
+# getting BIEN occurrence data for all species
+# making a function to loop through all species in my data frame and get their occurrences from BIEN
+occurrences <- function(spp) {
+  
+  list_of_occ <- list()
+  
+  for(i in 1:length(spp)){
+    df <- BIEN_occurrence_species(species = spp[[i]],
+                                    cultivated = TRUE,
+                                    all.taxonomy = TRUE,
+                                    native.status = TRUE,
+                                    observation.type = TRUE,
+                                    political.boundaries = TRUE)
+    list_of_occ[[i]] <- df
+  }
+  return(list_of_occ)
+}
+# store the results of the function
+# change "species#" and "df_list#" to match the subset of species I want to work with
+df_list5 <- occurrences(spp = species6)
+
+
+
+# checking some specific species
+Picea_glauca <- BIEN_occurrence_species(species = "Picea glauca",
+                                                cultivated = TRUE,
+                                                all.taxonomy = TRUE,
+                                                native.status = TRUE,
+                                                observation.type = TRUE,
+                                                political.boundaries = TRUE)
+Picea_laxa <- BIEN_occurrence_species(species = "Picea laxa",
+                                        cultivated = TRUE,
+                                        all.taxonomy = TRUE,
+                                        native.status = TRUE,
+                                        observation.type = TRUE,
+                                        political.boundaries = TRUE)
+Kobresia_tibetica <- BIEN_occurrence_species(species = "Kobresia tibetica",
+                                            cultivated = TRUE,
+                                            all.taxonomy = TRUE,
+                                            native.status = TRUE,
+                                            observation.type = TRUE,
+                                            political.boundaries = TRUE)
+Carex_bigelowii <- BIEN_occurrence_species(species = "Carex bigelowii",
+                                                    cultivated = TRUE,
+                                                    all.taxonomy = TRUE,
+                                                    native.status = TRUE,
+                                                    observation.type = TRUE,
+                                                    political.boundaries = TRUE)
+Deschampsia_antarctica <- BIEN_occurrence_species(species = "Deschampsia antarctica",
+                                            cultivated = TRUE,
+                                            all.taxonomy = TRUE,
+                                            native.status = TRUE,
+                                            observation.type = TRUE,
+                                            political.boundaries = TRUE)
+Lonicera_hispida <- BIEN_occurrence_species(species = "Lonicera hispida",
+                                                       cultivated = TRUE,
+                                                       all.taxonomy = TRUE,
+                                                       native.status = TRUE,
+                                                       observation.type = TRUE,
+                                                       political.boundaries = TRUE)
+Hylocomium_splendens <- BIEN_occurrence_species(species = "Hylocomium splendens",
+                                                 cultivated = TRUE,
+                                                 all.taxonomy = TRUE,
+                                                 native.status = TRUE,
+                                                 observation.type = TRUE,
+                                                 political.boundaries = TRUE)
+
+
+
+# setting up map parameters
+mp <- map('world', fill = TRUE, plot=F) 
+SP <- map2SpatialPolygons(mp, IDs = mp$names, 
+                          proj4string = CRS("+proj=longlat +datum=WGS84"))
+DATA <- data.frame(seq_len(length(SP)), row.names = names(SP))
+SPDF <- SpatialPolygonsDataFrame(SP, data = DATA)
+
+# mapping some species
+# Carex bigelowii
+Carex_bigelowii <- effect %>%
+  filter(Genus_Species == "Carex_bigelowii")
+spplot(SPDF, col.regions = "transparent", colorkey = FALSE,
+       par.settings = list(axis.line = list(col = "transparent"))) +
+  layer(panel.points(longitude, latitude, col="blue", pch=19), data=Carex_bigelowii_full) +
+  layer(panel.points(Longitude, Latitude, col="red", pch=19), data=Carex_bigelowii)
+# Betula nana
+Betula_nana <- effect %>%
+  filter(Genus_Species == "Betula_nana")
+spplot(SPDF, col.regions = "transparent", colorkey = FALSE,
+       par.settings = list(axis.line = list(col = "transparent"))) +
+  layer(panel.points(longitude, latitude, col="blue", pch=19), data=Betula_nana_full) +
+  layer(panel.points(Longitude, Latitude, col="red", pch=19), data=Betula_nana)
+# Deschampsia antarctica
+Deschampsia_antarctica <- effect %>%
+  filter(Genus_Species == "Deschampsia_antarctica")
+spplot(SPDF, col.regions = "transparent", colorkey = FALSE,
+       par.settings = list(axis.line = list(col = "transparent"))) +
+  layer(panel.points(longitude, latitude, col="blue", pch=19), data=Deschampsia_antarctica_full) +
+  layer(panel.points(Longitude, Latitude, col="red", pch=19), data=Deschampsia_antarctica)
+# Lonicera hispida
+Lonicera_hispida <- effect %>%
+  filter(Genus_Species == "Lonicera_hispida")
+spplot(SPDF, col.regions = "transparent", colorkey = FALSE,
+       par.settings = list(axis.line = list(col = "transparent"))) +
+  layer(panel.points(longitude, latitude, col="blue", pch=19), data=Lonicera_hispida_full) +
+  layer(panel.points(Longitude, Latitude, col="red", pch=19), data=Lonicera_hispida)
+# Hylocomium splendens
+Hylocomium_splendens <- effect %>%
+  filter(Genus_Species == "Hylocomium_splendens")
+spplot(SPDF, col.regions = "transparent", colorkey = FALSE,
+       par.settings = list(axis.line = list(col = "transparent"))) +
+  layer(panel.points(longitude, latitude, col="blue", pch=19), data=Hylocomium_splendens_full) +
+  layer(panel.points(Longitude, Latitude, col="red", pch=19), data=Hylocomium_splendens)
+
+# old map
+#map('world', fill = TRUE, col= "grey", bg = "light blue") 
+#points(cbind(Carex_bigelowii_full$longitude,
+#             Carex_bigelowii_full$latitude),
+#       col = "red",
+#       pch = 20,
+#       cex = 1) 
+#points(cbind(Carex_bigelowii$Longitude,
+#             Carex_bigelowii$Latitude),
+#       col = "blue",
+#       pch = 20,
+#       cex = 1) 
+
+
+
+
+### occurrence data - GBIF ###
+taxonKey <- name_backbone("Carex bigelowii")$usageKey
+occ_download(pred("taxonKey", 2722383))
+x <- occ_download_get('0248073-230224095556074') %>%
+  occ_download_import()
+
+plot(map_fetch(taxonKey = 2722383, bin = "hex", srs="EPSG:3857",
+               hexPerTile = 30, style = "purpleYellow-noborder.poly"))
 
 
 
@@ -83,116 +244,5 @@ points(cbind(Lonicera_hispida$Longitude,Lonicera_hispida$Latitude),
        col = "blue",
        pch = 20,
        cex = 1)
-
-
-
-
-### occurrence data - BIEN ###
-# Carex bigelowii
-Carex_bigelowii <- effect %>%
-  filter(Genus_Species == "Carex_bigelowii")
-Carex_bigelowii_full <- BIEN_occurrence_species(species = "Carex bigelowii",
-                                                    cultivated = TRUE,
-                                                    all.taxonomy = TRUE,
-                                                    native.status = TRUE,
-                                                    observation.type = TRUE,
-                                                    political.boundaries = TRUE)
-# Betula nana
-Betula_nana <- effect %>%
-  filter(Genus_Species == "Betula_nana")
-Betula_nana_full <- BIEN_occurrence_species(species = "Betula nana",
-                                                cultivated = TRUE,
-                                                all.taxonomy = TRUE,
-                                                native.status = TRUE,
-                                                observation.type = TRUE,
-                                                political.boundaries = TRUE)
-# Deschampsia antarctica
-Deschampsia_antarctica <- effect %>%
-  filter(Genus_Species == "Deschampsia_antarctica")
-Deschampsia_antarctica_full <- BIEN_occurrence_species(species = "Deschampsia antarctica",
-                                            cultivated = TRUE,
-                                            all.taxonomy = TRUE,
-                                            native.status = TRUE,
-                                            observation.type = TRUE,
-                                            political.boundaries = TRUE)
-
-# Lonicera hispida
-Lonicera_hispida <- effect %>%
-  filter(Genus_Species == "Lonicera_hispida")
-Lonicera_hispida_full <- BIEN_occurrence_species(species = "Lonicera hispida",
-                                                       cultivated = TRUE,
-                                                       all.taxonomy = TRUE,
-                                                       native.status = TRUE,
-                                                       observation.type = TRUE,
-                                                       political.boundaries = TRUE)
-Hylocomium_splendens <- effect %>%
-  filter(Genus_Species == "Hylocomium_splendens")
-Hylocomium_splendens_full <- BIEN_occurrence_species(species = "Hylocomium splendens",
-                                                 cultivated = TRUE,
-                                                 all.taxonomy = TRUE,
-                                                 native.status = TRUE,
-                                                 observation.type = TRUE,
-                                                 political.boundaries = TRUE)
-
-
-
-# setting up map parameters
-mp <- map('world', fill = TRUE, plot=F) 
-SP <- map2SpatialPolygons(mp, IDs = mp$names, 
-                          proj4string = CRS("+proj=longlat +datum=WGS84"))
-DATA <- data.frame(seq_len(length(SP)), row.names = names(SP))
-SPDF <- SpatialPolygonsDataFrame(SP, data = DATA)
-
-# mapping
-# Carex bigelowii
-spplot(SPDF, col.regions = "transparent", colorkey = FALSE,
-       par.settings = list(axis.line = list(col = "transparent"))) +
-  layer(panel.points(longitude, latitude, col="blue", pch=19), data=Carex_bigelowii_full) +
-  layer(panel.points(Longitude, Latitude, col="red", pch=19), data=Carex_bigelowii)
-# Betula nana
-spplot(SPDF, col.regions = "transparent", colorkey = FALSE,
-       par.settings = list(axis.line = list(col = "transparent"))) +
-  layer(panel.points(longitude, latitude, col="blue", pch=19), data=Betula_nana_full) +
-  layer(panel.points(Longitude, Latitude, col="red", pch=19), data=Betula_nana)
-# Deschampsia antarctica
-spplot(SPDF, col.regions = "transparent", colorkey = FALSE,
-       par.settings = list(axis.line = list(col = "transparent"))) +
-  layer(panel.points(longitude, latitude, col="blue", pch=19), data=Deschampsia_antarctica_full) +
-  layer(panel.points(Longitude, Latitude, col="red", pch=19), data=Deschampsia_antarctica)
-# Lonicera hispida
-spplot(SPDF, col.regions = "transparent", colorkey = FALSE,
-       par.settings = list(axis.line = list(col = "transparent"))) +
-  layer(panel.points(longitude, latitude, col="blue", pch=19), data=Lonicera_hispida_full) +
-  layer(panel.points(Longitude, Latitude, col="red", pch=19), data=Lonicera_hispida)
-# Hylocomium splendens
-spplot(SPDF, col.regions = "transparent", colorkey = FALSE,
-       par.settings = list(axis.line = list(col = "transparent"))) +
-  layer(panel.points(longitude, latitude, col="blue", pch=19), data=Hylocomium_splendens_full) +
-  layer(panel.points(Longitude, Latitude, col="red", pch=19), data=Hylocomium_splendens)
-
-# old map
-#map('world', fill = TRUE, col= "grey", bg = "light blue") 
-#points(cbind(Carex_bigelowii_full$longitude,
-#             Carex_bigelowii_full$latitude),
-#       col = "red",
-#       pch = 20,
-#       cex = 1) 
-#points(cbind(Carex_bigelowii$Longitude,
-#             Carex_bigelowii$Latitude),
-#       col = "blue",
-#       pch = 20,
-#       cex = 1) 
-
-
-
-
-### occurrence data - GBIF ###
-taxonKey <- name_backbone("Carex bigelowii")$usageKey
-occ_download(pred("taxonKey", 2722383))
-x <- occ_download_get('0248073-230224095556074') %>%
-  occ_download_import()
-
-plot(map_fetch(taxonKey = 2722383, bin = "hex", srs="EPSG:3857",
-               hexPerTile = 30, style = "purpleYellow-noborder.poly"))
 
 
